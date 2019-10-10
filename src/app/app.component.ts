e76a33faf7c88b869097e18db399eb643086d9c6
babamba <orochi13@naver.com>
2019-10-10 15:11:00 +0900

    다시

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
+
+// "start": "ng serve --proxy-config ./proxy.config.json",
 export class AppComponent {
   title = "angular-exam";
 }
